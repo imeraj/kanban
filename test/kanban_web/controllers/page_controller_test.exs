e9db206554f40c7010930dbeebf9913648f6bc32
Meraj Molla <meraj.enigma@gmail.com>
2026-03-25 06:13:34 -0400

    failing test step

--- test/kanban_web/controllers/page_controller_test.exs
+++ test/kanban_web/controllers/page_controller_test.exs
@@ -4,5 +4,6 @@ defmodule KanbanWeb.PageControllerTest do
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
     assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert false
   end
 end
