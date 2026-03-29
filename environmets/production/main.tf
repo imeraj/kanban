1ef4f623a3c437f70e0784523b0d5e79323deedd
Meraj Molla <meraj.enigma@gmail.com>
2026-03-29 17:21:11 -0400

    vpc, subnet, ssh key-gen

--- environmets/production/main.tf
+++ environmets/production/main.tf
@@ -1,15 +1,22 @@
 # in environments/production/main.tf
 
 module "swarm" {
-  source = "../../modules/cloud/aws/compute/swarm"
+  source           = "../../modules/cloud/aws/compute/swarm"
+  private_key_path = "${path.module}/private_key.pem"
 }
 
-import {
-  to = module.swarm.aws_instance.my_swarm
-  id = "i-0040ff555d3c957f9"
-}
+# commented out import blocks so that we don't try to re-import
+
+# import {
+#   to = module.swarm.aws_instance.my_swarm
+#   id = "i-0040ff555d3c957f9"
+# }
 
 # import {
 #   to = module.swarm.aws_security_group.swarm_sg
 #   id = "sg-0844606df17c97bdd"
 # }
+
+output "swarm_ssh_command" {
+  value = module.swarm.ssh_command
+}
--- modules/cloud/aws/compute/swarm/main.tf
+++ modules/cloud/aws/compute/swarm/main.tf
@@ -6,21 +6,70 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.13.1"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
   }
+}
+
+data "aws_vpc" "main" {
+  filter {
+    name   = "isDefault"
+    values = ["true"]
+  }
+}
+
+data "aws_subnets" "main_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+}
+
+# RSA key with 4096 rsa bits
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
+}
+
+resource "local_sensitive_file" "private_key" {
+  filename        = var.private_key_path
+  content         = tls_private_key.rsa.private_key_pem
+  file_permission = "0400"
+}
+
+resource "aws_key_pair" "deployer_key" {
+  key_name   = "swarm-key"
+  public_key = tls_private_key.rsa.public_key_openssh
 }
 
 resource "aws_instance" "my_swarm" {
   ami               = "ami-0f373f45fcf743d30"
   availability_zone = "ca-central-1b"
   instance_type     = "t3.micro"
-  key_name          = "swarm-key"
-  subnet_id         = "subnet-0f7554b80df0ea7dc"
+  key_name          = aws_key_pair.deployer_key.key_name
+  subnet_id         = data.aws_subnets.main_subnets.ids[0]
   tags = {
     "Name" = "docker-swarm-manager"
   }
   vpc_security_group_ids = [
     aws_security_group.swarm_sg.id
   ]
+  user_data = <<-EOF
+            #!/usr/bin/env bash
+
+            sudo dnf update -y && \
+            sudo dnf install -y docker && \
+            sudo systemctl start docker && \
+            sudo systemctl enable docker && \
+            sudo usermod -a -G docker ec2-user && \
+            newgrp docker
+            EOF
 }
 
 resource "aws_security_group" "swarm_sg" {
@@ -84,5 +133,5 @@ resource "aws_security_group" "swarm_sg" {
   name     = "launch-wizard-1"
   tags     = {}
   tags_all = {}
-  vpc_id   = "vpc-0ad38d0286d84e487"
+  vpc_id   = data.aws_vpc.main.id
 }
