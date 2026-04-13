588fdd4e34d7e9b57bac5bdb1b32b9efe4819296
Meraj Molla <meraj.enigma@gmail.com>
2026-04-13 09:39:53 -0400

    autoscaling

--- modules/cloud/aws/compute/swarm/autoscaling.tf
+++ modules/cloud/aws/compute/swarm/autoscaling.tf
@@ -22,13 +22,13 @@ resource "aws_launch_template" "swarm_node" {
   }
 }
 
-
 resource "aws_autoscaling_group" "main" {
   name = local.asg_name
 
   vpc_zone_identifier = data.aws_subnets.main_subnets.ids
   max_size            = var.number_of_nodes + 4
-  min_size            = var.number_of_nodes
+  min_size            = 2
+  desired_capacity    = 2
   health_check_type   = "EC2"
 
   termination_policies = ["NewestInstance"]
@@ -44,4 +44,84 @@ resource "aws_autoscaling_group" "main" {
     version = "$Latest"
   }
   depends_on = [aws_ssm_parameter.swarm_token]
+}
+
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "scale_up"
+  scaling_adjustment     = 2
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.main.name
+}
+
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "scale_down"
+  scaling_adjustment     = -2
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.main.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu" {
+  alarm_name          = "high-cpu-usage"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "45"
+  alarm_actions       = [aws_autoscaling_policy.scale_up.arn]
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.main.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_cpu" {
+  alarm_name          = "low-cpu-usage"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "35"
+  alarm_actions       = [aws_autoscaling_policy.scale_down.arn]
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.main.name
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "autoscaling_terminate_event_rule" {
+  name        = "autoscaling-terminate-event-rule"
+  description = "Trigger on autoscaling termination events"
+  event_pattern = jsonencode({
+    source        = ["aws.autoscaling"],
+    "detail-type" = ["EC2 Instance Terminate Successful"],
+    detail = {
+      AutoScalingGroupName = [aws_autoscaling_group.main.name]
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "stop_instances" {
+  target_id = "SwarmLeave"
+  arn       = "arn:aws:ssm:ca-central-1::document/AWS-RunShellScript"
+  input = jsonencode({
+    commands = [
+      "DOWN_NODE_IDS=$(docker node ls | grep 'Down' | awk '{print $1}')",
+      "docker node demote $DOWN_NODE_IDS",
+      "docker node rm $DOWN_NODE_IDS",
+      "NODES=$(docker node ls | awk 'NR > 1' | wc -l)",
+      "docker service update kanban_web --replicas $NODES"
+    ],
+  })
+
+  rule     = aws_cloudwatch_event_rule.autoscaling_terminate_event_rule.name
+  role_arn = aws_iam_role.event_bridge_role.arn
+
+  run_command_targets {
+    key    = "tag:Name"
+    values = ["docker-swarm-manager"]
+  }
 }
--- modules/cloud/aws/compute/swarm/iam.tf
+++ modules/cloud/aws/compute/swarm/iam.tf
@@ -34,6 +34,15 @@ resource "aws_iam_policy" "ssm_policy" {
         Effect   = "Allow",
         Action   = "ec2:CreateTags",
         Resource = "*"
+      },
+      {
+        Sid    = "EventBridge",
+        Effect = "Allow",
+        Action = [
+          "ssm:UpdateInstanceInformation",
+          "ec2messages:*"
+        ],
+        Resource = "*"
       }
     ]
   })
@@ -64,4 +73,43 @@ resource "aws_iam_role_policy_attachment" "ssm_role_policy_attachment" {
 resource "aws_iam_instance_profile" "main_profile" {
   name = "DockerSwarmSSMInstanceProfile"
   role = aws_iam_role.ssm_role.name
+}
+
+resource "aws_iam_policy" "event_bridge_policy" {
+  name        = "EventBridgePolicy"
+  description = "Event Bridge"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid      = "EventBridgeSendSSMCommand",
+        Effect   = "Allow",
+        Action   = "ssm:SendCommand",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "event_bridge_role" {
+  name = "EventBridgeRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "event_bridge_attachment" {
+  policy_arn = aws_iam_policy.event_bridge_policy.arn
+  role       = aws_iam_role.event_bridge_role.name
 }
